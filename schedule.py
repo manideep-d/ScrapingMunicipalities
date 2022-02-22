f2573f453dc6589f542de9d0f47c5a620c15af05
Manideep Dara <manideep@Manideeps-MacBook-Air.local>
2022-02-22 13:11:32 -0500

    added minutes

--- schedule.py
+++ schedule.py
@@ -13,8 +13,8 @@ def send_request():
 if __name__ == "__main__":
     subprocess.run("scrapyd-deploy", shell=True, universal_newlines=True)
     scheduler = TwistedScheduler(timezone=pytz.timezone('US/Eastern'))
-    # cron trigger that schedules job every every 20 minutues on weekdays
-    scheduler.add_job(send_request, 'cron', year=2022, month=2, day=22, hour=13)
+    # cron trigger that schedules jobxs
+    scheduler.add_job(send_request, 'cron', year=2022, month=2, day=22, hour=13, minute= 20)
     # start the scheduler
     scheduler.start()
     reactor.run()
